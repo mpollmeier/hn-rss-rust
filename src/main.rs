f46ab383c591452ec2a5f7b017ad50c1012f5b4c
Michael Pollmeier <michael@michaelpollmeier.com>
2017-11-17 18:25:10 +1300

    construct vector of description contents

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,42 @@
 extern crate hyper;
 extern crate xml;
 
-use std::io::Read;
-use hyper::Client;
+// use std::io::Read;
+// use hyper::Client;
 // use std::io;
 // use hyper::client::Response;
 
 use std::fs::File;
 use std::io::BufReader;
-use xml::reader::{EventReader, XmlEvent};
+use xml::reader;
+use xml::writer;
 
 fn main() {
     // let body = get().unwrap();
     // let body = read_file("/home/mp/hn-index.rss").unwrap();
     // println!("{}", body);
 
-    let file = File::open("/home/mp/hn-index.short.rss").unwrap();
+    let file = File::open("hn-index.short.rss").unwrap();
     let file = BufReader::new(file);
-    let parser = EventReader::new(file);
+    let parser = reader::EventReader::new(file);
 
-    let mut depth = 0;
+    // let mut outfile = File::create("output.xml").unwrap();
+    // let mut writer = writer::EmitterConfig::new().perform_indent(true).create_writer(&mut outfile);
+
+    let descriptions = extract_description_cdatas(parser);
+
+    println!("{}", descriptions.len());
+}
+
+fn extract_description_cdatas(parser: reader::EventReader<BufReader<File>>) -> Vec<String> {
+    let mut contents: Vec<String> = Vec::new();
     for e in parser {
         match e {
-            Ok(XmlEvent::StartElement { name, .. }) => {
-                println!("{}+{}", indent(depth), name);
-                depth += 1;
+            Ok(reader::XmlEvent::StartElement { name, .. }) => {
+                let inside_description = name.local_name == "description";
             }
-            Ok(XmlEvent::EndElement { name }) => {
-                depth -= 1;
-            }
-            Ok(XmlEvent::Characters(chars)) => {
-                println!("{}{}", indent(depth), chars);
-            }
-            Ok(XmlEvent::CData(chars)) => {
-                println!("{}{}", indent(depth), chars);
+            Ok(reader::XmlEvent::CData(chars)) => {
+                contents.push(chars);
             }
             Err(e) => {
                 println!("Error: {}", e);
@@ -42,13 +45,20 @@ fn main() {
             _ => {}
         }
     }
+    contents
 }
 
-fn indent(size: usize) -> String {
-    const INDENT: &'static str = "    ";
-    (0..size).map(|_| INDENT)
-             .fold(String::with_capacity(size*INDENT.len()), |r, s| r + s)
+// TODO: construct Vec<Article> from description cdata
+struct Article { 
+    title: String,
+    link: String
 }
+
+// fn indent(size: usize) -> String {
+//     const INDENT: &'static str = "    ";
+//     (0..size).map(|_| INDENT)
+//              .fold(String::with_capacity(size*INDENT.len()), |r, s| r + s)
+// }
 
 // fn get() -> Result<String, hyper::Error> {
 //     let client = Client::new();
