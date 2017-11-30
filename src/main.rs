2cf9a306d7cc159627d0e884066f2f0a44ff4210
Michael Pollmeier <michael@michaelpollmeier.com>
2017-11-30 20:22:04 +1300

    parsing with select works

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 extern crate hyper;
 extern crate xml;
+extern crate select;
 
 // use std::io::Read;
 // use hyper::Client;
@@ -10,6 +11,9 @@ use std::fs::File;
 use std::io::BufReader;
 use xml::reader;
 // use xml::writer;
+
+use select::document::Document;
+use select::predicate::{Predicate, Class, Name};
 
 fn main() {
     // let body = get().unwrap();
@@ -23,55 +27,85 @@ fn main() {
     // let mut outfile = File::create("output.xml").unwrap();
     // let mut writer = writer::EmitterConfig::new().perform_indent(true).create_writer(&mut outfile);
 
-    let descriptions = extract_description_cdatas(parser);
-    let articles: Vec<Article> = descriptions.into_iter().flat_map(parse_description_cdata).collect();
+    // let descriptions: Vec<String> = extract_description_cdatas(parser);
+    // let descriptions = descriptions.into_iter().map(|desc|Document::from(&desc[..]));
+    let articles: Vec<Article> = extract_description_cdatas(parser)
+        .into_iter()
+        .map(|desc|Document::from(&desc[..]))
+        .flat_map(parse_description_document)
+        .collect();
+    
     for x in &articles {
         println!("{:?}", x);
     }
+
 }
 
-fn parse_description_cdata(cdata: String) -> Vec<Article> {
-    let parser = reader::EventReader::from_str(&cdata);
-
+fn parse_description_document(document: Document) -> Vec<Article> {
     let mut articles: Vec<Article> = Vec::new();
-    let mut inside_storylink = false;
-    let mut current_story = String::new();
-    let mut current_href = String::new();
-    for e in parser {
-        match e {
-            Ok(reader::XmlEvent::StartElement { name, attributes, .. }) => {
-                if attributes.iter().find(|attr| attr.value == "storylink").is_some() {
-                    inside_storylink = true;
-                }
-                if inside_storylink && name.local_name == "a" {
-                    let href = attributes.iter().find(|attr| attr.name.local_name == "href");
-                    if href.is_some() {
-                        current_href = href.unwrap().value.clone();
-                    }
-                }
+
+    for node in document.find(Class("storylink").descendant(Name("a"))) {
+        articles.push(
+            Article {
+                title: node.text(),
+                link: node.attr("href").unwrap().to_string()
             }
-            Ok(reader::XmlEvent::Characters(chars)) => {
-                if inside_storylink {
-                    current_story = String::from(chars);
-                }
-            }
-            Ok(reader::XmlEvent::EndElement { name }) => {
-                if name.local_name == "span" && inside_storylink {
-                    articles.push(
-                        Article {
-                            title: current_story.clone(),
-                            link: current_href.clone()
-                        }
-                    );
-                    inside_storylink = false;
-                }
-            }
-            _ => {}
-        }
+        );
     }
 
     articles
 }
+
+// fn parse_description_cdata2(cdata: String) -> Vec<Article> {
+//     // println!("X {}", cdata);
+//     let parser = reader::EventReader::from_str(&cdata);
+
+//     let mut articles: Vec<Article> = Vec::new();
+//     let mut inside_storylink = false;
+//     let mut current_story = String::new();
+//     let mut current_href = String::new();
+//     for e in parser {
+//         match e {
+//             Ok(reader::XmlEvent::StartElement { name, attributes, .. }) => {
+//                 let href = attributes.iter().find(|attr| attr.name.local_name == "href");
+//                 if href.is_some() {
+//                     // println!("{}", href.unwrap());
+//                 }
+//                 if attributes.iter().find(|attr| attr.value == "storylink").is_some() {
+//                     inside_storylink = true;
+//                 }
+//                 if inside_storylink && name.local_name == "a" {
+//                     let href = attributes.iter().find(|attr| attr.name.local_name == "href");
+//                     if href.is_some() {
+//                         current_href = href.unwrap().value.clone();
+//                     }
+//                 }
+//             }
+//             Ok(reader::XmlEvent::Characters(chars)) => {
+//                 if inside_storylink {
+//                     current_story = String::from(chars);
+//                 }
+//             }
+//             Ok(reader::XmlEvent::EndElement { name }) => {
+//                 if name.local_name == "span" && inside_storylink {
+//                     articles.push(
+//                         Article {
+//                             title: current_story.clone(),
+//                             link: current_href.clone()
+//                         }
+//                     );
+//                     inside_storylink = false;
+//                 }
+//             }
+//             Err(e) => {
+//                 println!("err: {}", e);
+//             }
+//             _ => {}
+//         }
+//     }
+
+//     articles
+// }
 
 fn extract_description_cdatas(parser: reader::EventReader<BufReader<File>>) -> Vec<String> {
     let mut contents: Vec<String> = Vec::new();
